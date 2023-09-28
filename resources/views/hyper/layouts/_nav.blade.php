c374910fced56bb8e9cb82dcdc735c9d35860046
qifang280 <qifang280@gmail.com>
2023-09-28 22:01:06 +0800

    独角卡自定义

--- resources/views/hyper/layouts/_nav.blade.php
+++ resources/views/hyper/layouts/_nav.blade.php
@@ -5,9 +5,47 @@
             <img src="{{ picture_ulr(dujiaoka_config_get('img_logo')) }}" height="36">
             <div class="logo-title">{{ dujiaoka_config_get('text_logo') }}</div>
         </a>
-        <a class="btn btn-outline-primary" href="{{ url('order-search') }}">
-            <i class="noti-icon uil-file-search-alt search-icon"></i>
-            查询订单
-        </a>
+        <ul class="list-unstyled topbar-right-menu float-right mb-0">
+
+                        <li class="notification-list">
+                            <a class="nav-link right-bar-toggle" href="https://chatgpt.567bus.shop/"  target="_blank">
+                                <i class="noti-icon mdi mdi-cow"></i>
+                                本站免费ChatGpt
+                            </a>
+                        </li>
+
+            <li class="notification-list">
+                <a class="nav-link right-bar-toggle" href="https://t.me/bus567buhuotz" target="_blank">
+                    <i class="noti-icon mdi mdi-bell-outline"></i>
+                    补货通知
+                </a>
+            </li>
+
+            <li class="notification-list">
+                <a class="nav-link right-bar-toggle" href="https://t.me/bus5678" target="_blank">
+                    <i class="noti-icon mdi mdi-telegram"></i>
+                    官方群组
+                </a>
+            </li>
+
+            <li class="notification-list">
+                <a class="nav-link right-bar-toggle" href="https://t.me/shantianliang_bot" target="_blank">
+                    <i class="noti-icon mdi mdi-telegram"></i>
+                    联系店主
+                </a>
+            </li>
+
+            <li class="notification-list">
+                <a class="nav-link right-bar-toggle" href="https://www.567bus.shop/order-search">
+                    <i class="noti-icon mdi mdi-magnify search-icon"></i>
+                    查询订单
+                </a>
+            </li>
+
+        </ul>
+<!--        <a class="btn btn-outline-primary" href="{{ url('order-search') }}">-->
+<!--            <i class="noti-icon uil-file-search-alt search-icon"></i>-->
+<!--            查询订单-->
+<!--        </a>-->
     </div>
 </div>
