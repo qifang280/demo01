223f0c0729093f1f177216dad22287bd2f3daf1a
qifang280 <qifang280@gmail.com>
2023-10-05 21:56:08 +0800

    独角卡自定义-通知

--- resources/views/hyper/layouts/_nav.blade.php
+++ resources/views/hyper/layouts/_nav.blade.php
@@ -8,35 +8,35 @@
         <ul class="list-unstyled topbar-right-menu float-right mb-0">
             <li class="notification-list">
                 <a class="nav-link right-bar-toggle" href="https://chatgpt.567bus.shop/"  target="_blank">
-                    <i class="noti-icon mdi mdi-cow">::before</i>
+                    <i class="noti-icon mdi mdi-cow"></i>
                     本站免费ChatGpt
                 </a>
             </li>
 
             <li class="notification-list">
                 <a class="nav-link right-bar-toggle" href="https://t.me/bus567buhuotz" target="_blank">
-                    <i class="noti-icon mdi mdi-bell-outline">::before</i>
+                    <i class="noti-icon mdi mdi-bell-outline"></i>
                     补货通知
                 </a>
             </li>
 
             <li class="notification-list">
                 <a class="nav-link right-bar-toggle" href="https://t.me/bus5678" target="_blank">
-                    <i class="noti-icon mdi mdi-telegram">::before</i>
+                    <i class="noti-icon mdi mdi-telegram"></i>
                     官方群组
                 </a>
             </li>
 
             <li class="notification-list">
                 <a class="nav-link right-bar-toggle" href="https://t.me/shantianliang_bot" target="_blank">
-                    <i class="noti-icon mdi mdi-telegram">::before</i>
+                    <i class="noti-icon mdi mdi-telegram"></i>
                     联系店主
                 </a>
             </li>
 
             <li class="notification-list">
                 <a class="nav-link right-bar-toggle" href="https://www.567bus.shop/order-search">
-                    <i class="noti-icon mdi mdi-magnify search-icon">::before</i>
+                    <i class="noti-icon mdi mdi-magnify search-icon"></i>
                     查询订单
                 </a>
             </li>
